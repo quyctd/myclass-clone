3b200504db01f06d5a01d196f88fb5c57f17599e
quyctd <quy.dc98@gmail.com>
2018-07-23 22:39:09 +0700

    Prepared to deploy

--- myclass/settings.py
+++ myclass/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'fbx!+93@sqng#o#*(47=43bov0s90keqtk#l+pj*abntdx2_8d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "myclassvn.herokuapp.com"]
 
 
 # Application definition
