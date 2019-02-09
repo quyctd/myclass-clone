45ab89d1002e5aa476ac476c59b9ad9eac73ae3f
Quý Đinh <quy.dc98@gmail.com>
2019-02-09 21:45:49 +0700

    Deploy

--- myclass/settings.py
+++ myclass/settings.py
@@ -14,6 +14,8 @@ try:
 except ImportError:
     from ConfigParser import RawConfigParser
 import os
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,12 +28,12 @@ config = RawConfigParser()
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fbx!+93@sqng#o#*(47=43bov0s90keqtk#l+pj*abntdx2_8d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "myclassvn.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "hustclass.herokuapp.com"]
 
 
 # Application definition
@@ -80,16 +82,19 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myclass.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
     }
 }
 
@@ -152,9 +157,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = ( os.path.join('static'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
