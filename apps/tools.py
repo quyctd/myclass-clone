d04a84f1f66ddf8c1031017c2e3844b9de2b6042
quyctd <quy.dc98@gmail.com>
2019-03-02 01:19:22 +0700

    Fix bug in tools.py

--- apps/tools.py
+++ apps/tools.py
@@ -10,13 +10,15 @@ cloudinary.config(
 )
 
 def pre_upload_cover_course(sender, instance, *args, **kwargs):
-    cover_photo = instance.anh_cover.open()
-    cloud_img = uploader.upload(cover_photo)
-    cloud_link = cloud_img['url']
-    instance.cover_link = cloud_link
+    if instance.anh_cover:
+        cover_photo = instance.anh_cover.open()
+        cloud_img = uploader.upload(cover_photo)
+        cloud_link = cloud_img['url']
+        instance.cover_link = cloud_link
 
 def pre_upload_avatar_image(sender, instance, *args, **kwargs):
-    avatar = instance.avatar.open()
-    cloud_avatar = uploader.upload(avatar)
-    cloud_link = cloud_avatar['url']
-    instance.avatar_link = cloud_link
+    if instance.avatar:
+        avatar = instance.avatar.open()
+        cloud_avatar = uploader.upload(avatar)
+        cloud_link = cloud_avatar['url']
+        instance.avatar_link = cloud_link
